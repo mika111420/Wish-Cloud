41779d9185e5f86162e6bceb9043c5c218399c2f
Heidy Beltran <HeidyBeltran@Alains-MacBook-Air.local>
2023-09-13 18:57:24 -0400

    added addtional comments to welcome page

--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Welcome() {
   return (
@@ -8,6 +9,34 @@ function Welcome() {
         Embrace the magic of special occasions by creating a wishlist. It's the
         first step towards turning your dreams into reality.
       </p>
+      <Link to="/Signup">
+        <button>🎁 Create WishCloud 🎁</button>
+      </Link>
+      <div>
+        <h2>Fast and cost-free </h2>
+        <p>
+          WishCloud allows you to swiftly create your wishlist at no cost and
+          easier than ever!
+        </p>
+      </div>
+      <div>
+        <h2> For every event </h2>
+        <p>Design your wishlist to suit any occasion of your choosing.</p>
+      </div>
+      <div>
+        <h2> With one click</h2>
+        <p>
+          With the WishCloud button you can add gift items to your wish list
+          with just one click.
+        </p>
+      </div>
+      <div>
+        <h2> Share easily</h2>
+        <p>
+          Share your wish list with family and friends via e-mail, messenger or
+          social media.
+        </p>
+      </div>
     </div>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,10 @@
 import React from "react";
-// import ProductList from "../components/ProductList";
-// import CategoryMenu from "../components/CategoryMenu";
-// import Cart from "../components/Cart";
 import Welcome from "../components/Welcome";
 
 const Home = () => {
   return (
     <div className="container">
       <Welcome />
-      {/* <CategoryMenu /> */}
-      {/* <ProductList />
-      <Cart /> */}
     </div>
   );
 };
