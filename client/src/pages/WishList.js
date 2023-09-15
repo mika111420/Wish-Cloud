937e4f3f3fa30a82ce4305acaf35d5165a8379f7
franko88bit <franks188@icloud.com>
2023-09-14 23:35:06 -0400

    updated

--- client/src/pages/WishList.js
+++ client/src/pages/WishList.js
@@ -1,23 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-import { useQuery, useMutation } from "@apollo/client";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { useQuery } from "@apollo/client";
 import { QUERY_USER } from "../utils/queries";
-import { ADD_ORDER } from "../utils/mutations";
-import { useSearchParams } from "react-router-dom";
 
-function WishList() {
-  //const [searchparams] = useSearchParams();
-  //console.log("hi there");
-  //console.log(searchparams.get("state_cart_arr"));
+function Wishlist() {
   const { data } = useQuery(QUERY_USER);
-  console.log (data);
   let user;
 
-  const [setData] = useMutation(ADD_ORDER);
-
   if (data) {
-    user = data.user.orders;
+    user = data.user;
   }
 
   return (
@@ -28,25 +21,44 @@ function WishList() {
         {user ? (
           <>
             <h2>
-              WishList for {user.firstName} {user.lastName}
+              Wishlist for {user.firstName} {user.lastName}
             </h2>
-            {user.orders.map((order) => (
-              <div key={order._id} className="my-2">
+            <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+                <ul>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-300'>
+                        <a className='flex justify-between items-center w-full text-white'
+                            href="https://www.linkedin.com/in/mikayla-rangel-8b013b291/">Linkedin<FaLinkedin size={30} /></a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-slate-600'>
+                        <a className='flex justify-between items-center w-full text-white'
+                            href="https://github.com/mika111420">GitHub<FaGithub size={30} /></a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-red-400'>
+                        <a className='flex justify-between items-center w-full text-white' href="mailto:mikaylar1202@gmail.com">Email<HiOutlineMail size={30} /></a>
+                    </li>
+                </ul>
+            </div>
+            {user.wishlist.map((wishlist) => (
+              <div key={wishlist._id} className="my-2">
                 <h3>
-                  {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
+                  {new Date(
+                    parseInt(wishlist.purchaseDate)
+                  ).toLocaleDateString()}
                 </h3>
                 <div className="flex-row">
-                  {order.products.map(({ _id, image, name, price }, index) => (
-                    <div key={index} className="card px-1 py-1">
-                      <Link to={`/products/${_id}`}>
-                        <img alt={name} src={`/images/${image}`} />
-                        <p>{name}</p>
-                      </Link>
-                      <div>
-                        <span>${price}</span>
+                  {wishlist.products.map(
+                    ({ _id, image, name, price }, index) => (
+                      <div key={index} className="card px-1 py-1">
+                        <Link to={`/products/${_id}`}>
+                          <img alt={name} src={`/images/${image}`} />
+                          <p>{name}</p>
+                        </Link>
+                        <div>
+                          <span>${price}</span>
+                        </div>
                       </div>
-                    </div>
-                  ))}
+                    )
+                  )}
                 </div>
               </div>
             ))}
@@ -57,4 +69,5 @@ function WishList() {
   );
 }
 
-export default WishList;
+export default Wishlist;
+
