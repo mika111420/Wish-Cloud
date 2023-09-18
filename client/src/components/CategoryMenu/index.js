ea5685f1d0de21e3d03eed2579047b7f234461e6
Mikayla <user@users-MBP.attlocal.net>
2023-09-18 18:10:58 -0400

    final styling for products

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -44,13 +44,14 @@ function CategoryMenu({ setdisplay }) {
 
   return (
     <div>
-      <h2>Select your special occasion:</h2>
+      <h2 className="text-3xl font-bold mt-4 mb-4">Select your special occasion:</h2>
       {categories.map((item) => (
         <button
           key={item._id}
           onClick={() => {
             handleClick(item._id);
           }}
+          className="bg-blue-500 text-white rounded-full px-4 py-2 m-2 font-semibold hover:bg-blue-600 sm:text-sm sm:px-3 sm:py-1 sm:m-1 md:text-base md:px-4 md:py-2 md:m-2 lg:text-lg lg:px-4 lg:py-2 lg:m-2"
         >
           {item.name}
         </button>
--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -6,9 +6,9 @@ import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 
-function ProductItem(item) {
+function ProductItem({item}) {
   const [state, dispatch] = useStoreContext();
-
+console.log(item)
   const {
     image,
     name,
@@ -41,19 +41,18 @@ function ProductItem(item) {
   }
 
   return (
-    <div className="card px-1 py-1">
+    <div className="bg-white rounded-lg p-4 shadow-md">
       <Link to={`/products/${_id}`}>
         <img
           alt={name}
           src={`${image}`}
-        />
-        <p>{name}</p>
+          className="w-full h-40 object-cover rounded-t-lg"/>
+        <h3 className="text-lg font-semibold mt-2">{name}</h3>
+        <p className="text-gray-700 text-sm mt-1">Price: ${price}</p>
+        <p className="text-gray-700 text-sm">Quantity: {quantity}</p>
       </Link>
-      <div>
-        <div>{quantity} {pluralize("item", quantity)} in stock</div>
-        <span>${price}</span>
-      </div>
-      <button onClick={addToCart}>Add to cart</button>
+      
+      <button onClick={addToCart} className="bg-blue-500 text-white rounded-full px-4 py-2 mt-2 font-semibold hover:bg-blue-600">Add to cart</button>
     </div>
   );
 }
